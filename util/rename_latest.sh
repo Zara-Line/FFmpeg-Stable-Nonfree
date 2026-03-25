e5e475954582edff3a22d67bd4b81163f1faf451
Zara Line <public-oss@mythview.ccwu.cc>
2026-03-25 23:28:23 +0800

    Delete releases syncing temporarily. Boost latest release update.

---
+++ util/rename_latest.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+if [[ $# -lt 2 ]]; then
+    echo "Missing arguments"
+    exit -1
+fi
+
+RELEASE_DIR="$(realpath "$1")"
+shift
+mkdir -p "$RELEASE_DIR"
+
+rm -rf repack_dir
+mkdir repack_dir
+trap "rm -rf repack_dir" EXIT
+
+while [[ $# -gt 0 ]]; do
+    INPUT="$1"
+    shift
+
+    (
+        set -e
+        REPACK_DIR="repack_dir/$BASHPID"
+        rm -rf "$REPACK_DIR"
+        mkdir "$REPACK_DIR"
+
+        # cp instead of uncompress
+        if [[ $INPUT == *.zip ]] || [[ $INPUT == *.tar.xz ]]; then
+            cp "$INPUT" "$REPACK_DIR/"
+        else
+            echo "Unknown input file type: $INPUT"
+            exit 1
+        fi
+
+        cd "$REPACK_DIR"
+
+        INAME="$(echo ffmpeg-*)"
+        TAGNAME="$(cut -d- -f2 <<<"$INAME")"
+
+        if [[ $TAGNAME == N ]]; then
+            TAGNAME="master"
+        elif [[ $TAGNAME == n* ]]; then
+            TAGNAME="$(sed -re 's/([0-9]+\.[0-9]+).*/\1/' <<<"$TAGNAME")"
+        fi
+
+        if [[ "$INAME" =~ -[0-9]+-g ]]; then
+            ONAME="ffmpeg-$TAGNAME-latest-$(cut -d- -f5- <<<"$INAME")"
+        else
+            ONAME="ffmpeg-$TAGNAME-latest-$(cut -d- -f3- <<<"$INAME")"
+        fi
+
+        ONAME="${ONAME%-[0-9]*.[0-9]*}"    # Cut off redundant version tag.
+
+        # rename (mv) instead of compress
+        if [[ $INPUT == *.zip ]]; then
+            ONAME="$ONAME.zip"
+        elif [[ $INPUT == *.tar.xz ]]; then
+            ONAME="$ONAME.tar.xz"
+        else
+            echo "Unknown input file type: $INPUT"
+            exit 1
+        fi
+
+        mv "$INAME" "$RELEASE_DIR/$ONAME"
+
+        rm -rf "$REPACK_DIR"
+    ) &
+
+    while [[ $(jobs | wc -l) -gt 3 ]]; do
+        wait %1
+    done
+done
+
+while [[ $(jobs | wc -l) -gt 0 ]]; do
+    wait %1
+done
+rm -rf repack_dir
